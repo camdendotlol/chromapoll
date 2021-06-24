a43ea57e5b1c2a60352be0f32e4a95a5e76ba719
Camden Mecklem <c@camdenmecklem.com>
2021-06-23 21:35:23 -0400

    set up react for frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App: React.FC = () => (
+  <p>Chromapoll!</p>
+)
+
+export default App
---
+++ webpack.config.ts
@@ -0,0 +1,40 @@
+import path from 'path'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const config = {
+  entry: './frontend/src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist', 'frontend'),
+    filename: 'ui.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|js)$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react',
+            '@babel/preset-typescript'
+          ]
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.js']
+  },
+  devServer: {
+    proxy: {
+      '/api': `http://localhost:${process.env.PORT}`
+    },
+    contentBase: path.resolve(__dirname, 'dist', 'frontend'),
+    publicPath: '/',
+    historyApiFallback: { index: 'index.html' }
+  }
+}
+
+module.exports = config
