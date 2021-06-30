f6ed9de3bb83fd70c9435e383aeb0c45c2050355
Camden Mecklem <c@camdenmecklem.com>
2021-06-30 18:55:18 -0400

    make navbar a bit nicer

--- frontend/src/components/Navbar/ChromaLogo.tsx
+++ frontend/src/components/Navbar/ChromaLogo.tsx
@@ -1,19 +1,25 @@
 import React, { CSSProperties } from 'react'
+import styled from 'styled-components'
 
-const styles: CSSProperties = {
-  fontWeight: 1000,
-  fontSize: '3rem',
-  margin: 0,
-  paddingLeft: '10px',
-  background: 'linear-gradient(to top right, #36A4C7, #B13E84)',
-  backgroundClip: 'text',
-  WebkitTextFillColor: 'transparent',
-  display: 'inline-block',
-  WebkitBackgroundClip: 'text'
-}
+const LogoText = styled.p`
+  fontWeight: 1000;
+  font-size: 1.5rem;
+  margin: 0;
+  padding-left: 10px;
+  background: linear-gradient(to top right, #1dfded, #ef45fc);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  display: inline-block;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
 const ChromaLogo: React.FC = () => {
   return (
-    <p style={styles}>C</p>
+    <LogoText>Chromapoll</LogoText>
   )
 }
 
--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,25 +1,36 @@
 import React, { CSSProperties } from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 import ChromaLogo from './ChromaLogo'
 
 const styles: CSSProperties = {
   position: 'fixed',
   top: '0px',
   background: '#4C4E52',
-  height: '60px',
+  // height: '60px',
   width: '100%',
   margin: '0',
   zIndex: 1000,
   boxShadow: '0 0 3px #4e4a4a',
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center'
 }
+
+const NavbarContent = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  align-content: center;
+`
 
 const Navbar: React.FC = () => {
   return (
     <div style={styles}>
-      <Link to='/'><ChromaLogo /></Link>
+      <NavbarContent>
+        <Link to='/'><ChromaLogo /></Link>
+      </NavbarContent>
     </div>
   )
 }
